78202aad0be367fa84441916245b082e43f49a98
alboy77 <al.boucetta@yahoo.com>
2023-05-24 16:38:03 -0400

    file structure, more code, css

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://stackoverflow.com">Stack Overflow</a>
+          </li>
+          <li>
+            <a href="https://github.com/albo1">GitHub</a>
+          </li>
+          <li>
+            <a href="https://linkedin.com">LinkedIn</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header() {
+    return (
+        <header className="header">
+       <h1>Al Boucetta</h1>
+        </header>
+    );
+  }
+  
+export default Header;
