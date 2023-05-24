9722757196b3fcb923ef906f5f5dc94f03e34c2e
alboy77 <al.boucetta@yahoo.com>
2023-05-24 16:21:50 -0400

    profile container

---
+++ src/utils/helpers.js
@@ -0,0 +1,14 @@
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  
+  export function validateName(name) {
+    const re = /^([a-zA-Z]{2,})$/;
+    return re.test(String(name).toLowerCase());
+  }
+  
+  export function validateMessage(message) {
+    const re = /^([a-zA-Z]{2,})$/;
+    return re.test(String(message).toLowerCase());
+  }
