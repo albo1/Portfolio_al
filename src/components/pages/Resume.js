68fc8df54b5ec747c14c90fc1dce58d5632aa9b7
alboy77 <al.boucetta@yahoo.com>
2023-05-24 23:39:47 -0400

    figured out the resume wooohoo

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import resumePDF from '../images/AL_RESUME_2023.pdf'
 
 export default function Home() {
   return (
     <div>
       <h1>Resume</h1>
-      <a href="" target="_blank" rel="">Download Resume ⬇️</a>
+      <a href={resumePDF} download="AL_RESUME_2023.pdf" target="_blank" rel="noopener noreferrer">Download Resume ⬇️</a>
     </div>
   );
 }
