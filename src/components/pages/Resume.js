9da5aae8d2929b453e7747fbf9197187d397d260
alboy77 <al.boucetta@yahoo.com>
2023-05-24 22:04:41 -0400

    tried with resume

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <a href="" target="_blank" rel="">Download Resume ⬇️</a>
+    </div>
+  );
+}
